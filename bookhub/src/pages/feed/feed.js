4ebfc26d47b7d32aaa440052b17e3be54d504d21
anhhuynhUNC <anhhuynh@live.unc.edu>
2023-04-25 01:59:04 -0400

    moved feed

--- bookhub/src/pages/feed/feed.js
+++ bookhub/src/pages/feed/feed.js
@@ -1,16 +1,18 @@
-import Navbar from "./navbar/navbar";
-import TabHeader from "./tabHeader";
-import Explore from "./tabs/exploreTab";
-import Bookshelf from "./tabs/bookshelfTab";
-import Account from "./tabs/accountTab";
+import Navbar from "../components/navbar/navbar";
+import TabHeader from "../components/tabHeader";
+import Explore from "../components/tabs/exploreTab";
+import Bookshelf from "../components/tabs/bookshelfTab";
+import Account from "../components/tabs/accountTab";
 import { useState, useEffect } from "react";
 import { getBook, getData } from "../../firebaseAPI/firebaseAPI";
-import GuestNavbar from "./guest_navbar/guest_navbar";
-
+import { updateFromSignup } from "./loginHelper";
+import GuestNavbar from "../components/guest_navbar/guest_navbar";
 import { default_fetch } from "../../utils/bookmatch";
+import SignupPage from "../components/Login/SignupPage";
 export default function Feed() {
     let [current, setCurrent] = useState(1);
     let [default_data, setDefaultData] = useState([]);
+    let [auth, setAuth] = useState(false);
     //if not logged in just do top 5 genre
 
     //use effect load current selection of books
@@ -23,9 +25,11 @@ export default function Feed() {
             case 1:
                 return <Explore default_data={default_data}></Explore>
             case 2:
-                return <Bookshelf></Bookshelf>
+                return <Bookshelf auth = {auth}></Bookshelf>
             case 3:
                 return <Account></Account>
+            case 4:
+                return <SignupPage back = {setCurrent} setCurrent={()=>{updateFromSignup(setCurrent,setAuth)}}></SignupPage>
             default:
                 return <div></div>
         }
@@ -33,10 +37,11 @@ export default function Feed() {
 
     return ( //here need to check if someone is logged in, display guest navbar if not
         <div className={"topContainer"}>
-            <GuestNavbar></GuestNavbar>
+            {/* login navbar with toggle to signup page */}
+            {(!auth && current !== 4)? <GuestNavbar setAuth = {setAuth} setCurrent = {setCurrent}></GuestNavbar> : <Navbar/>}
             <div className={"main"}>
                 <div className={"tabHeaderContainer"}>
-                    <TabHeader setCurrent={setCurrent} current={current}></TabHeader>
+                    {current !== 4 ? <TabHeader setCurrent={setCurrent} current={current}></TabHeader> :<></>}
                 </div>
                 <div className={"tabContainer"}>
                     {page()}
