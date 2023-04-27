de98a7c5de29ab6a049ca33736ea0374cfad4a4a
anhhuynhUNC <anhhuynh@live.unc.edu>
2023-04-27 00:22:09 -0400

    added auth attr

--- bookhub/src/pages/components/exploreTabContainer/explore_default.js
+++ bookhub/src/pages/components/exploreTabContainer/explore_default.js
@@ -1,6 +1,6 @@
 import { default_fetch } from "../../../utils/bookmatch";
 import { useState, useEffect } from "react";
-import RowContainer from "../atom/RowContainer";
+import RowContainer from "../atom/row/RowContainer";
 
 export default function Explore_default(props) {
     if (props == undefined || props.data == undefined) return ;
@@ -11,7 +11,7 @@ export default function Explore_default(props) {
             {props.data.map((val, i) => {
                 return <div className={"rowContainer"} key = {i}>
                     <h3>{val.attr == undefined ? "" : val.attr}</h3>
-                    <RowContainer data={val.data} key={i} uid={props.uid}></RowContainer>
+                    <RowContainer data={val.data} key={i} uid={props.uid} auth={props.auth}></RowContainer>
                 </div>
             })}
         </>
