12401c4bbb60d421d66ba4a79fbb347204a03583
anhhuynhUNC <anhhuynh@live.unc.edu>
2023-04-27 00:19:50 -0400

    reorganized folders and remove unnecessary tags

---
+++ bookhub/src/pages/components/atom/row/RowContainer.js
@@ -0,0 +1,49 @@
+import Row from "./rowAtom"
+import { getRowList } from "../../../../utils/atomUtil"
+import { useState } from "react";
+
+export default function RowContainer(props) {
+    console.log(props.data);
+    const [car, setCar] = useState(0);
+    let data = getRowList(props.data);
+    const [hasReachEnd, setHasReachEnd] = useState(false);
+
+    function handleRight() {
+        if (car == data.length - 1) {
+            setCar(0);
+            setHasReachEnd(true);
+        } else {
+            setCar(car + 1);
+        }
+    }
+
+    function handleLeft() {
+        if (car == 0) {
+            setCar(data.length - 1);
+        } else {
+            setCar(car - 1);
+        }
+    }
+
+    function showInBeginning() {
+        if (car == 0 && hasReachEnd) {
+            return true;
+        }
+        if (car == 0 && !hasReachEnd) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+    return (
+        <>
+            {showInBeginning() ? <button className="leftbutton" onClick={handleLeft}>{'<<'}</button> : <></>}
+            <div className={"row"}>
+                <Row data={data[car]} uid={props.uid} auth={props.auth}></Row>
+            </div>
+            <button className="rightbutton" onClick={handleRight}>{'>>'}</button>
+        </>
+    )
+}
---
+++ bookhub/src/pages/components/atom/row/rowAtom.js
@@ -0,0 +1,16 @@
+import Book from "../book/bookAtom"
+
+export default function Row(props) {
+    if (props.data == undefined) return;
+    return (
+        props.data.map((data, i) => {
+            let volumeInfo = data.volumeInfo;
+            let name = volumeInfo.title;
+            let description = volumeInfo.description
+            let coverId = volumeInfo.imageLinks.thumbnail;
+            let authors = volumeInfo.authors;
+            return <Book description={description} name={name} coverId={coverId} key={i} authors={authors} uid={props.uid} auth={props.auth}></Book>
+        })
+    )
+
+}
