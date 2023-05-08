5887f03dd86ad7f57082636f6cceac3f029ebe33
Michael Trentini <mtren@live.unc.edu>
2023-05-08 17:57:41 -0400

    alerts

--- bookhub/src/firebaseAPI/firebaseAPI.js
+++ bookhub/src/firebaseAPI/firebaseAPI.js
@@ -76,10 +76,10 @@ function CreateNewUser(uid, name, age, email, yesGenres, noGenres, pages) {
     return update(ref(db), updates).catch((error) => { console.log(error) });
 }
 
-function addBookToUser(uid, name, auth) {
+ function addBookToUser(uid, name, auth) {
     if (!auth) {
         //alert replace later
-        alert("Not Logged In!")
+        //alert("Not Logged In!")
         return;
     }
     const db = getDatabase();
@@ -98,7 +98,7 @@ function addBookToUser(uid, name, auth) {
 function addBookToCompleted(uid, name, auth) {
     if (!auth) {
         //alert replace later
-        alert("Not Logged In!")
+        //alert("Not Logged In!")
         return;
     }
     const db = getDatabase();
@@ -118,7 +118,7 @@ function addBookToCompleted(uid, name, auth) {
 function addBookToForLater(uid, name, auth) {
     if (!auth) {
         //alert replace later
-        alert("Not Logged In!")
+        //alert("Not Logged In!")
         return;
     }
     const db = getDatabase();
@@ -138,7 +138,7 @@ function addBookToForLater(uid, name, auth) {
 function addBookToLightReading(uid, name, auth) {
     if (!auth) {
         //alert replace later
-        alert("Not Logged In!")
+        //alert("Not Logged In!")
         return;
     }
     const db = getDatabase();
