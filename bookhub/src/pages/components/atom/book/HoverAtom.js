364b09bcc9bba7b1468ad5bf8989aeeab62fd73a
Michael Trentini <mtren@live.unc.edu>
2023-05-08 17:46:26 -0400

    sign up

--- bookhub/src/pages/components/atom/book/HoverAtom.js
+++ bookhub/src/pages/components/atom/book/HoverAtom.js
@@ -6,9 +6,9 @@ export default function HoverAtom(props) {
     return (
         <div style={props.style} className={"hoverInfo"}>
             <h4>Title: {props.name}
-                <button id="completed" style={{ width: "75px", height: "28px", marginLeft: "100px" }} onClick={() => { addBookToCompleted(props.uid, props.name, props.auth); }}> Completed </button>
-                <button id="completed" style={{ width: "75px", height: "28px", marginLeft: "175px" }} onClick={() => { addBookToForLater(props.uid, props.name, props.auth) }}> For Later </button>
-                <button id="completed" style={{ width: "75px", height: "28px", marginLeft: "250px" }} onClick={() => { addBookToLightReading(props.uid, props.name, props.auth) }}> Light Reading </button>
+                <button id="completed" style={{ width: "75px", fontFamily: "Rockwell", fontSize: "10px", height: "28px", marginLeft: "100px" }} onClick={() => { addBookToCompleted(props.uid, props.name, props.auth); }}> Completed </button>
+                <button id="completed" style={{ width: "75px", fontFamily: "Rockwell", fontSize: "10px", height: "28px", marginLeft: "175px" }} onClick={() => { addBookToForLater(props.uid, props.name, props.auth) }}> For Later </button>
+                <button id="completed" style={{ width: "75px", fontFamily: "Rockwell", fontSize: "10px", height: "28px", marginLeft: "250px" }} onClick={() => { addBookToLightReading(props.uid, props.name, props.auth) }}> Light Reading </button>
             </h4>
             <h4>Author: {props.authors === undefined ? "" : props.authors[0]}</h4>
             <p>Description: {props.description}</p>
