09495b6b87ad4b5d56d18551288a0f0c53abd452
crbenn <72770269+crbenn@users.noreply.github.com>
2023-05-08 19:00:20 -0400

    Update HoverAtom.js

--- bookhub/src/pages/components/atom/book/HoverAtom.js
+++ bookhub/src/pages/components/atom/book/HoverAtom.js
@@ -11,13 +11,13 @@ export default function HoverAtom(props) {
                 <div className="reactionContainer">
                     <img src={sadFace} id="sadFace" onClick={() => { addBooktoDislike(props.uid, props.name, props.auth) }}></img>
                     <img src={happyFace} id="happyFace" onClick={() => { addBookToForLater(props.uid, props.data, props.name, props.auth) }}></img>
-                    <button id="completed" style={{ width: "75px", fontFamily: "Rockwell", fontSize: "10px", height: "28px", marginLeft: "100px" }} onClick={() => { addBookToCompleted(props.uid, props.data, props.name, props.auth); }}> Completed </button>
-                    <button id="completed" style={{ width: "75px", fontFamily: "Rockwell", fontSize: "10px", height: "28px", marginLeft: "100px" }} onClick={() => { addBookToLightReading(props.uid, props.data, props.name, props.auth) }}> Light Reading </button>
+                    <button id="completed" style={{ width: "75px", fontFamily: "Rockwell", fontSize: "10px", height: "28px", marginLeft: "400px" }} onClick={() => { addBookToCompleted(props.uid, props.data, props.name, props.auth); }}> Completed </button>
+                    <button id="completed" style={{ width: "75px", fontFamily: "Rockwell", fontSize: "10px", height: "28px", marginLeft: "400px" }} onClick={() => { addBookToLightReading(props.uid, props.data, props.name, props.auth) }}> Light Reading </button>
                 </div> : <></>}
             <div className="infoContainer">
                 <h4>Title: {props.name}</h4>
                 <h4>Author: {props.authors === undefined ? "" : props.authors[0]}</h4>
-                <p>Description: {props.description}</p>
+                <p style={{ marginRight: "80px" }}>Description: {props.description}</p>
 
             </div>
         </div>
